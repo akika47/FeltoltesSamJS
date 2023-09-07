2079c5b55a25114224d7710a3a2968acfff77645
akika47 <61090999+akika47@users.noreply.github.com>
2023-09-07 11:00:57 +0200

    Add files via upload

---
+++ KapacitSzamoloJS/script.js
@@ -0,0 +1,45 @@
+function Szamolas(){
+    let KapacitNagysag = document.getElementById("KapacitNagysag").value;
+    let KapacitFajta = document.getElementById("KapacitFajta").value;
+    let AtviteliNagysag = document.getElementById("AtviteliNagysagSlider").value;
+    let AtviteliFajta = document.getElementById("AtviteliFajta").value;
+
+    if (KapacitFajta =="GB")
+    {
+        KapacitNagysag = KapacitNagysag * 1000;
+    }
+    else if (kapacitFajta == "TB")
+    {
+        KapacitNagysag = KapacitNagysag * 1000000;
+    }
+
+    if (AtviteliFajta == "GBps")
+    {
+        AtviteliNagysag = AtviteliNagysag * 1000;
+    }
+    else if (AtviteliFajta == "KBps")
+    {
+        AtviteliNagysag = AtviteliNagysag / 1000;
+    }
+    else if (AtviteliFajta == "Mbps")
+    {
+        AtviteliNagysag = AtviteliNagysag / 8;
+    }
+
+    let eredmeny = KapacitNagysag / AtviteliNagysag;
+
+    document.getElementById("EredmenyKiir").innerHTML = eredmeny + "sec";
+
+
+
+}
+
+function Kiir(){
+    let slider = document.getElementById("AtviteliNagysagSlider").value;
+    document.getElementById("SliderErtek").innerHTML = slider;
+
+}
+
+document.querySelectorAll('input[type="range"]').forEach((input) => { 
+    input.addEventListener('mousedown',  () => window.getSelection().removeAllRanges());
+});
